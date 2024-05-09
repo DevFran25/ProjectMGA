52f5688efa7563cd9ef79e9974b215dbb5094fda
DevFran25 <developerfran25@gmail.com>
2024-05-09 10:09:26 -0500

    Agregue clase ciente en modelo

---
+++ src/main/java/pe/edu/utp/model/Cliente.java
@@ -0,0 +1,64 @@
+package pe.edu.utp.model;
+
+public class Cliente {
+
+    //Atributos
+    private String identificacion;
+    private String tipo_cliente;
+    private String nombre;
+    private String apellidos;
+    private String email;
+    private String celular;
+
+    //Constructor Vacio
+    public Cliente() {
+    }
+
+
+    //Constructor Lleno
+    public Cliente(String identificacion, String tipo_cliente, String nombre, String apellidos, String email, String celular) {
+        this.identificacion = identificacion;
+        this.tipo_cliente = tipo_cliente;
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.email = email;
+        this.celular = celular;
+    }
+
+    //Get
+    public String getIdentificacion() {
+        return identificacion;
+    }
+
+    public String getTipo_cliente() {
+        return tipo_cliente;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getCelular() {
+        return celular;
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" +
+                "identificacion='" + identificacion + '\'' +
+                ", tipo_cliente='" + tipo_cliente + '\'' +
+                ", nombre='" + nombre + '\'' +
+                ", apellidos='" + apellidos + '\'' +
+                ", email='" + email + '\'' +
+                ", celular='" + celular + '\'' +
+                '}';
+    }
+}
