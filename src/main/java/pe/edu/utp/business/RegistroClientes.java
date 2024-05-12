973b455e217ce2df6b6eb550b8f5796db5c2123c
DevFran25 <developerfran25@gmail.com>
2024-05-12 17:13:40 -0500

    Se agrego listar clientes

--- src/main/java/pe/edu/utp/business/RegistroClientes.java
+++ src/main/java/pe/edu/utp/business/RegistroClientes.java
@@ -6,9 +6,12 @@ import pe.edu.utp.service.ClientesService;
 import pe.edu.utp.util.AppConfig;
 import pe.edu.utp.util.DataAccessMariaDB;
 import pe.edu.utp.util.ErrorLog;
+import pe.edu.utp.utils.TextUTP;
+
 import javax.naming.NamingException;
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.List;
 
 public class RegistroClientes {
 
@@ -48,4 +51,37 @@ public class RegistroClientes {
         }
     }
 
+    //Listar Clientes
+    public String getHtmlListarClientes() throws IOException, SQLException {
+        // Cargar plantilla principal
+        String filename = "src\\main\\resources\\web\\clients.html";
+        String html = TextUTP.read(filename);
+
+        // Cargar plantilla para los item
+        String filenameItems = "src\\main\\resources\\templates\\cliente_listado.html";
+        String htmlItem = TextUTP.read(filenameItems);
+
+
+        // Recorrer la lista de Clientes
+        StringBuilder itemsHtml = new StringBuilder();
+
+        // Listar
+        List<Cliente> listaClientes = busquedaServiceClientes.getAllClientes();
+
+        for (Cliente cliente : listaClientes) {
+
+            //Tabla Clientes
+            String item = htmlItem.replace("${identificacion}", cliente.getIdentificacion())
+                    .replace("${tipo_cliente}", cliente.getTipo_cliente())
+                    .replace("${nombre}",cliente.getNombre())
+                    .replace("${apellidos}", cliente.getApellidos())
+                    .replace("${email}", cliente.getEmail())
+                    .replace("${celular}", cliente.getCelular());
+            itemsHtml.append(item);
+        }
+        // Reemplazar en la plantilla principal
+        String reporteHtml = html.replace("${itemsClientes}", itemsHtml.toString());
+
+        return reporteHtml;
+    }
 }
