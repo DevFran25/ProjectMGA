3a92e4b445f93ff15fba309eb7a21c496aec080e
DevFran25 <developerfran25@gmail.com>
2024-05-15 11:50:36 -0500

    Se agrego una carpeta para la base de datos y los procedimientos

---
+++ src/main/resources/Base_de_datos/projectsdb.sql
@@ -0,0 +1,381 @@
+CREATE DATABASE  IF NOT EXISTS `projectsdb` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `projectsdb`;
+-- MySQL dump 10.13  Distrib 8.0.36, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: projectsdb
+-- ------------------------------------------------------
+-- Server version	8.0.37
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `actividad`
+--
+
+DROP TABLE IF EXISTS `actividad`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `actividad` (
+  `id_actividad` int NOT NULL AUTO_INCREMENT,
+  `id_proyecto` char(10) NOT NULL,
+  `nombre` varchar(30) NOT NULL,
+  `estado` varchar(20) NOT NULL,
+  PRIMARY KEY (`id_actividad`),
+  KEY `id_proyecto` (`id_proyecto`),
+  CONSTRAINT `actividad_ibfk_1` FOREIGN KEY (`id_proyecto`) REFERENCES `proyecto` (`id_proyecto`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `actividad`
+--
+
+LOCK TABLES `actividad` WRITE;
+/*!40000 ALTER TABLE `actividad` DISABLE KEYS */;
+/*!40000 ALTER TABLE `actividad` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `avance`
+--
+
+DROP TABLE IF EXISTS `avance`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `avance` (
+  `id_avance` int NOT NULL AUTO_INCREMENT,
+  `id_proyecto` char(10) NOT NULL,
+  `dni_colaborador` char(8) NOT NULL,
+  `progreso` varchar(30) NOT NULL,
+  PRIMARY KEY (`id_avance`),
+  KEY `id_proyecto` (`id_proyecto`),
+  KEY `id_trabajador` (`dni_colaborador`),
+  KEY `dni_colaborador` (`dni_colaborador`),
+  CONSTRAINT `avance_ibfk_2` FOREIGN KEY (`dni_colaborador`) REFERENCES `colaborador` (`dni_colaborador`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `avance`
+--
+
+LOCK TABLES `avance` WRITE;
+/*!40000 ALTER TABLE `avance` DISABLE KEYS */;
+/*!40000 ALTER TABLE `avance` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `cliente`
+--
+
+DROP TABLE IF EXISTS `cliente`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `cliente` (
+  `id_cliente` int NOT NULL AUTO_INCREMENT,
+  `identificacion` varchar(20) NOT NULL COMMENT 'RUC O DNI',
+  `tipo_cliente` varchar(20) NOT NULL COMMENT 'Persona Jurídica o Natural',
+  `nombre` varchar(20) DEFAULT NULL,
+  `apellidos` varchar(30) DEFAULT NULL,
+  `email` char(9) NOT NULL,
+  `celular` varchar(20) NOT NULL,
+  PRIMARY KEY (`id_cliente`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cliente`
+--
+
+LOCK TABLES `cliente` WRITE;
+/*!40000 ALTER TABLE `cliente` DISABLE KEYS */;
+INSERT INTO `cliente` VALUES (1,'71484506','Natural','Francisco','Aquino','martin@gm','945784124');
+/*!40000 ALTER TABLE `cliente` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `colaborador`
+--
+
+DROP TABLE IF EXISTS `colaborador`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `colaborador` (
+  `dni_colaborador` char(8) NOT NULL,
+  `nombres` varchar(25) NOT NULL,
+  `apellidos` varchar(30) NOT NULL,
+  `telefono` char(10) NOT NULL,
+  `email` varchar(30) NOT NULL,
+  `cargo` varchar(20) NOT NULL,
+  PRIMARY KEY (`dni_colaborador`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `colaborador`
+--
+
+LOCK TABLES `colaborador` WRITE;
+/*!40000 ALTER TABLE `colaborador` DISABLE KEYS */;
+INSERT INTO `colaborador` VALUES ('78450689','Martin','Aquino','987451202','FranAquino25@gmail.com','Gerente');
+/*!40000 ALTER TABLE `colaborador` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `entragable`
+--
+
+DROP TABLE IF EXISTS `entragable`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `entragable` (
+  `id_entregable` int NOT NULL AUTO_INCREMENT,
+  `id_proyecto` char(10) NOT NULL,
+  `nombre` varchar(30) NOT NULL,
+  `fecha` date NOT NULL,
+  `file` varchar(50) NOT NULL,
+  PRIMARY KEY (`id_entregable`),
+  KEY `id_proyecto` (`id_proyecto`),
+  CONSTRAINT `fk_entregable_proyecto` FOREIGN KEY (`id_proyecto`) REFERENCES `proyecto` (`id_proyecto`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `entragable`
+--
+
+LOCK TABLES `entragable` WRITE;
+/*!40000 ALTER TABLE `entragable` DISABLE KEYS */;
+/*!40000 ALTER TABLE `entragable` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `proyecto`
+--
+
+DROP TABLE IF EXISTS `proyecto`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `proyecto` (
+  `id_proyecto` char(10) NOT NULL,
+  `id_cliente` int NOT NULL,
+  `dni_colaborador` char(8) NOT NULL,
+  `nombre` varchar(30) NOT NULL,
+  `ubicacion` varchar(30) NOT NULL,
+  `costo` float NOT NULL,
+  `fecha_inicio` date NOT NULL,
+  `fecha_fin` date NOT NULL,
+  `estado` varchar(20) NOT NULL,
+  `foto` varchar(30) NOT NULL,
+  PRIMARY KEY (`id_proyecto`),
+  KEY `id_cliente` (`id_cliente`),
+  KEY `dni_colaborador` (`dni_colaborador`),
+  CONSTRAINT `proyecto_ibfk_1` FOREIGN KEY (`id_cliente`) REFERENCES `cliente` (`id_cliente`),
+  CONSTRAINT `proyecto_ibfk_2` FOREIGN KEY (`dni_colaborador`) REFERENCES `colaborador` (`dni_colaborador`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `proyecto`
+--
+
+LOCK TABLES `proyecto` WRITE;
+/*!40000 ALTER TABLE `proyecto` DISABLE KEYS */;
+INSERT INTO `proyecto` VALUES ('P01',1,'78450689','Proyecto 01','Chiclayo',40.5,'2024-05-14','2024-05-31','Pendiente','images.jpg'),('P02',1,'78450689','Proyecto 02','Lambayeque',87.5,'2024-05-14','2024-05-31','Pendiente','images.jpg'),('P03',1,'78450689','Proyecto 03','Chiclayo',5000,'2024-05-15','2024-05-29','Pendiente','Arquitectura.png');
+/*!40000 ALTER TABLE `proyecto` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `usuario`
+--
+
+DROP TABLE IF EXISTS `usuario`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `usuario` (
+  `id_usuario` int NOT NULL AUTO_INCREMENT,
+  `dni_colaborador` char(8) NOT NULL,
+  `username` varchar(20) NOT NULL,
+  `password` varchar(32) NOT NULL,
+  PRIMARY KEY (`id_usuario`),
+  KEY `dni_colaborador` (`dni_colaborador`),
+  CONSTRAINT `usuario_ibfk_1` FOREIGN KEY (`dni_colaborador`) REFERENCES `colaborador` (`dni_colaborador`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `usuario`
+--
+
+LOCK TABLES `usuario` WRITE;
+/*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
+INSERT INTO `usuario` VALUES (1,'78450689','Francisco','0101');
+/*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping events for database 'projectsdb'
+--
+
+--
+-- Dumping routines for database 'projectsdb'
+--
+/*!50003 DROP PROCEDURE IF EXISTS `listarClientes` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'NO_AUTO_VALUE_ON_ZERO' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `listarClientes`()
+BEGIN
+SELECT identificacion, tipo_cliente,nombre, apellidos,email,celular   FROM cliente;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `listarColaboradores` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'NO_AUTO_VALUE_ON_ZERO' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `listarColaboradores`()
+BEGIN
+	SELECT dni_colaborador, nombres, apellidos, telefono, email, cargo   FROM colaborador;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `listarProyectos` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'NO_AUTO_VALUE_ON_ZERO' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `listarProyectos`()
+BEGIN
+SELECT id_proyecto,id_cliente,dni_colaborador,nombre,ubicacion,costo,fecha_inicio,fecha_fin,estado,foto FROM proyecto;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `RegistrarCliente` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'NO_AUTO_VALUE_ON_ZERO' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `RegistrarCliente`(IN `p_identificacion` VARCHAR(20), IN `p_tipo_cliente` VARCHAR(20), IN `p_nombre` VARCHAR(20), IN `p_apellidos` VARCHAR(30), IN `p_email` VARCHAR(50), IN `p_celular` VARCHAR(20))
+BEGIN
+    INSERT INTO cliente (identificacion, tipo_cliente, nombre, apellidos, email, celular)
+    VALUES (p_identificacion, p_tipo_cliente, p_nombre, p_apellidos, p_email, p_celular);
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `RegistrarColaborador` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'NO_AUTO_VALUE_ON_ZERO' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `RegistrarColaborador`(IN `p_dni_colaborador` CHAR(8), IN `p_nombres` VARCHAR(25), IN `p_apellidos` VARCHAR(30), IN `p_telefono` CHAR(10), IN `p_email` VARCHAR(30), IN `p_cargo` VARCHAR(20))
+BEGIN
+    INSERT INTO colaborador (dni_colaborador, nombres, apellidos, telefono, email, cargo)
+    VALUES (p_dni_colaborador, p_nombres, p_apellidos, p_telefono, p_email, p_cargo);
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `RegistrarProyecto` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'NO_AUTO_VALUE_ON_ZERO' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `RegistrarProyecto`(IN `id_proyecto` CHAR(10), IN `id_cliente` INT, IN `dni_colaborador` CHAR(8), IN `nombre` VARCHAR(30), IN `ubicacion` VARCHAR(30), IN `costo` FLOAT, IN `fecha_inicio` DATE, IN `fecha_fin` DATE, IN `estado` VARCHAR(20), IN `foto` VARCHAR(30))
+BEGIN
+
+INSERT INTO proyecto (id_proyecto, id_cliente, dni_colaborador, nombre, ubicacion, costo, fecha_inicio, fecha_fin, estado, foto)
+    VALUES (id_proyecto, id_cliente, dni_colaborador, nombre, ubicacion, costo, fecha_inicio, fecha_fin, estado, foto);
+
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `RegistrarUsuario` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'NO_AUTO_VALUE_ON_ZERO' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `RegistrarUsuario`(IN `p_dni` CHAR(8), IN `p_username` VARCHAR(20), IN `p_password` VARCHAR(32))
+BEGIN
+    INSERT INTO usuario (dni_colaborador, username, password)
+    VALUES (p_dni, p_username, p_password);
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-05-15 11:42:28
